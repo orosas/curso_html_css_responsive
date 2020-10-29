f272672d635e574d4a3341e2eda3e671e34ddbba
orosas <jorosas@gmail.com>
2020-10-29 09:02:33 -0600

    Inicia refactor de CSS para Dakr mode

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+
+const elCheckbox = document.getElementById('checkbox');
+
+console.log('El checkbox es: ', elCheckbox);
+
+console.log(window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+
+// Nota: No funciona el hacer set de checbox checked ...........
+if(!window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    console.log('Dentro de if');
+    elCheckbox.setAttribute('checked', true);
+    console.log(elCheckbox);
+}
+
+elCheckbox.addEventListener('change', function(event) {
+    console.log(this.checked);
+    // if (this.checked) {
+    //     document.body.classList.add('is-dark-mode');
+    // } else{
+    //     document.body.classList.remove('is-dark-mode');
+    // }
+        document.body.classList.toggle('is-dark-mode');
+})
+
+// Nota: Continuar
+// 2:05:27
